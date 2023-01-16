15f0edcc3aad0a1400926c5ceaafa400ab480829
JohanLeon0914 <johanalbertols@ufps.edu.co>
2023-01-16 12:01:19 -0500

    agregando seccion de series y peliculas con un scroll bar

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import { Movie } from "../typing";
 import { useState, useEffect } from "react";
 import { baseUrl } from "../constants/movie";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiInformationCircle } from "react-icons/hi";
 
 interface Props {
   netflixOriginals: Movie[];
@@ -16,27 +18,35 @@ function Banner({ netflixOriginals }: Props) {
     );
   }, [netflixOriginals]);
 
-  console.log(movie)
+  console.log(movie);
 
   return (
-    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12">
+    <div className="flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[90vh] lg:justify-end lg:pb-12">
       <div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
-        <Image src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
-         layout='fill' 
-         alt='movie'
-         objectFit="cover"
-         />
-       </div>
+        <Image
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+          alt="movie"
+          objectFit="cover"
+        />
+      </div>
 
-        <h1 className="text-2xl font-bold lg:text-7xl md:text-4xl">
-            {movie?.title || movie?.original_name}
-        </h1>
-        <p className="max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">{movie?.overview}</p>
-        
-        <div>
-            <button className="bannerButton">Play</button>
-            <button className="bannerButton">More Info</button>
-        </div>
+      <h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
+        {movie?.title || movie?.original_name}
+      </h1>
+      <p className="max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl">
+        {movie?.overview}
+      </p>
+
+      <div className="flex space-x-3">
+        <button className="bannerButton bg-white text-black">
+          <BsFillPlayFill className="text-black md:h-7 md:w-7" />
+          Play
+        </button>
+        <button className="bannerButton bg-[gray]/70">
+          More Info <HiInformationCircle className="md:h-8 md:w-8" />
+        </button>
+      </div>
     </div>
   );
 }
